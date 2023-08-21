76f3aeb7b8f62d906adaebf46f9b1202bd0b311c
Pascal Schneider <80832353+padasch@users.noreply.github.com>
2023-08-21 17:03:26 +0200

    setup r file

---
+++ R/_setup.R
@@ -0,0 +1,23 @@
+# R functions
+sapply(
+  list.files(here::here("R"), 
+             pattern = "^[^_].*", 
+             full.names = TRUE), 
+  source
+)
+
+# Packages
+packages <- c(
+  "here", "tidyverse", "ggplot2", "patchwork"
+)
+
+# Install packages not yet installed
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+
+# Packages loading
+invisible(lapply(packages, library, character.only = TRUE))
+# conflict_prefer(name = "select", winner = "dplyr")
+# conflict_prefer(name = "filter", winner = "dplyr")
