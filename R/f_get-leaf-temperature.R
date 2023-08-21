39426c9d3859bf87245026e9eca108bebf77970b
Pascal Schneider <80832353+padasch@users.noreply.github.com>
2023-08-21 09:19:02 +0200

    initial population

---
+++ R/f_get-leaf-temperature.R
@@ -0,0 +1,12 @@
+f_get_leaf_temperature <- function(inn) {
+  
+  #' Get Leaf Temperature
+  #'
+  #' @param inn 
+  #'
+  #' @return Returns an object of class "?". Description of what the function returns
+  #' @examples
+  #' # Add some code illustrating how to use the function
+
+  print(inn)
+}
